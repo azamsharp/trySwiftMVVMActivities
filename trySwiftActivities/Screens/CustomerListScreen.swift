8d31b354c7ea7ac096a1608e563db5cbd368d5b7
Mohammad Azam <azamsharp@gmail.com>
2021-01-28 10:18:01 -0600

    trySwift activities

---
+++ trySwiftActivities/Screens/CustomerListScreen.swift
@@ -0,0 +1,20 @@
+//
+//  CustomerListScreen.swift
+//  trySwiftActivities
+//
+//  Created by Mohammad Azam on 1/28/21.
+//
+
+import SwiftUI
+
+struct CustomerListScreen: View {
+    var body: some View {
+        Text("Show List of Customers")
+    }
+}
+
+struct CustomerListScreen_Previews: PreviewProvider {
+    static var previews: some View {
+        CustomerListScreen()
+    }
+}
---
+++ trySwiftActivities/View Models/CustomerListViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  CustomerListViewModel.swift
+//  trySwiftActivities
+//
+//  Created by Mohammad Azam on 1/28/21.
+//
+
+import Foundation
+
+class CustomerListViewModel: ObservableObject {
+    
+    
+    
+}
